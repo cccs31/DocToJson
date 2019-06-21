bce67b1f866fa7db40ae676062b055e1090c4593
cccs31 <justinho37@gmail.com>
2019-06-20 20:50:45 -0700

    JSON with some unnecessary attributes removed

--- parse.js
+++ parse.js
@@ -8,18 +8,58 @@ if (fileToOpen === undefined) { // quit immediately if there is no 3rd argument
     return;
 }
 
-// extract the specified file to a directory '/file' in the current working directory
-const extract = require('extract-zip');
-extract('docs/' + fileToOpen, { dir: process.cwd() + '/file' }, function (err) {
-    if (err) throw err;
+// put document.xml into doc as a string
+const AdmZip = require('adm-zip');
+let zip = new AdmZip('docs/' + fileToOpen);
+let doc;
+zip.getEntries().forEach(zipEntry => {
+    if (zipEntry.entryName == 'word/document.xml')
+        doc = zipEntry.getData().toString('utf8');
 });
 
 const convert = require('xml-js');
+let i = 0;
+let options = {
+    compact: true,
+    spaces: 4,
+    ignoreDeclaration: true,
+    ignoreInstruction: true,
+    ignoreComment: true,
+    ignoreCdata: true,
+    ignoreDoctype: true,
+    /* attributesKey: 'type', textKey: 'value', */
+    /* attributeValueFn: function (value, name) {
+        if (name != 'w:t') return '';
+        if (value != 'w:val') {
+            // console.log(i++ + ' ' + value);
+            return 'wat';
+        }
+    }, */
+    /* attributeNameFn: function(name, value) {
+        if (name == 'w:p') {
+            console.log(i++ + ' ' + name);
+            return 'wot';
+        }
+    } */
+};
+let docObj = convert.xml2js(doc, options); // convert xml to js object
+
+// remove non-text xml tags from the converted js object
+function removeTags(docObj) {
+    if ('w:document' in docObj) {
+        if ('_attributes' in docObj['w:document']) {
+            // remove namespaces
+            delete docObj['w:document']['_attributes'];
+        }
+        if ('w:body' in docObj['w:document']) {
+            if ('w:sectPr' in docObj['w:document']['w:body']) {
+                delete docObj['w:document']['w:body']['w:sectPr'];
+            }
+        }
+    }
+}
+
+removeTags(docObj);
+
 const fs = require('fs');
-
-let xml = fs.readFileSync('file/word/document.xml', 'utf-8');
-let options = { compact: true, spaces: 4, ignoreDeclaration: true, ignoreInstruction: true, ignoreComment: true, ignoreCdata: true, ignoreDoctype: true, attributesKey: 'type', textKey: 'value' };
-let result = convert.xml2json(xml, options);
-// console.log(result);
-
-fs.writeFile('test.json', result, () => { });
+fs.writeFile('test.json', JSON.stringify(docObj, null, 4), (err) => { if (err) throw err; });
